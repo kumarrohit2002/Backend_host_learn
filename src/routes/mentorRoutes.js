8dce49f27ca8ae3536c4309a44dd89f8f5aa4587
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-09-21 16:38:37 +0530

    get mentor profile

--- src/routes/mentorRoutes.js
+++ src/routes/mentorRoutes.js
@@ -7,10 +7,10 @@ const {middleware} = require('../middleware/middleware');
 
 router.post('/create-or-update-profile',middleware,createOrUpdateMentorProfile);
 router.post('/getMentorProfile',middleware,getMentorProfile);
-router.post('/getAllMentorProfiles',getAllMentorProfiles);
+router.get('/getAllMentorProfiles',getAllMentorProfiles);
 router.post('/getprofile',getMentorProfileById);
 router.post('/searchMentor',searchMentor);
 router.post('/post-new-job',middleware,postJob);
-router.post('/get-all-job-posts',getAllPostJob);
+router.post('/get-all-job-posts',getAllPostJob);    
 
 module.exports = router;

2110b584d60020920b0e26ccc13d053f523e2a79
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-09-21 16:15:19 +0530

    comment auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const {
-    signup,
-    verifyOTP,
-    resendOTP,
-    login,
-    forgotPassword,
-    verifyPasswordOTP,
-    resetPassword,
-} = require('../controllers/authController');
+// const express = require('express');
+// const {
+//     signup,
+//     verifyOTP,
+//     resendOTP,
+//     login,
+//     forgotPassword,
+//     verifyPasswordOTP,
+//     resetPassword,
+// } = require('../controllers/authController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/verify-otp', verifyOTP);
-router.post('/resend-otp', resendOTP);  // New route for resending OTP
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/verify-password-otp', verifyPasswordOTP);
-router.post('/reset-password', resetPassword);
+// router.post('/signup', signup);
+// router.post('/verify-otp', verifyOTP);
+// router.post('/resend-otp', resendOTP);  // New route for resending OTP
+// router.post('/login', login);
+// router.post('/forgot-password', forgotPassword);
+// router.post('/verify-password-otp', verifyPasswordOTP);
+// router.post('/reset-password', resetPassword);
 
 
-module.exports = router;
+// module.exports = router;

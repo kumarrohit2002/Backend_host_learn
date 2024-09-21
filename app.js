4bcc5b49f0c973d4dc0e5f2cbc186c809953aaaa
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-09-21 10:40:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const PORT=process.env.PORT || 4000;
+
+app.use(express.json());
+
+require("./config/database").connect();
+
+app.listen(PORT,()=>{
+    console.log(`listening on port no : http://localhost:${PORT}`);
+});
+
+app.get('/',(req,res)=>{
+    res.send(`<h1>This is HomePage yaar</h1>`);
+})
+
+app.get('/getdata',(req,res)=>{
+    res.send({
+        Name:'rohit kumar',
+        Reg:'12201517'
+    })
+})
